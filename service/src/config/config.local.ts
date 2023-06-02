83bd1bf39fd1aebe5d1293ac326dceff0898e549
Han <>
2023-06-02 16:51:55 +0800

    no message

--- service/src/config/config.local.ts
+++ service/src/config/config.local.ts
@@ -1,20 +1,19 @@
 import { MidwayConfig } from '@midwayjs/core';
 
 export default {
-  client: {
-    // 数据库存连接配置
-    // host
-    host: '127.0.0.1',
-    // 端口号
-    port: '3306',
-    // 用户名
-    user: 'root',
-    // 密码
-    password: 'root',
-    // 数据库名
-    database: 'file',
-    charset: 'utf8mb4', //制定字符集用于保存emoji
-    dateStrings: true,
-    timezone: '+08:00',
-  },
+    client: { // 数据库存连接配置
+        // host
+        host: '127.0.0.1',
+        // 端口号
+        port: '3306',
+        // 用户名
+        user: 'root',
+        // 密码
+        password: 'root',
+        // 数据库名
+        database: 'template',
+        charset: "utf8mb4",//制定字符集用于保存emoji
+        dateStrings: true,
+        timezone: "+08:00"
+    },
 } as MidwayConfig;

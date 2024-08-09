f0fea7f8312a005de1959bddfbc8503833a8bb4e
Han <>
2024-08-09 16:16:16 +0800

    no message

--- service/src/middleware/report.middleware.ts
+++ service/src/middleware/report.middleware.ts
@@ -8,17 +8,27 @@ export class ReportMiddleware implements IMiddleware<Context, NextFunction> {
     return async (ctx: Context, next: NextFunction) => {
       try {
         await next();
-        // isCustom === true 自定义返回类型 
+        // isCustom === true 自定义返回类型
         if (!ctx.isCustom) {
           ctx.status = 200;
-          return ctx.body = { code: 200, data: ctx.body };
+          return (ctx.body = { code: 200, data: ctx.body });
         }
       } catch (error) {
-        ctx.logger.error('发生错误--', error, ctx.request.method, { body: ctx.request.body, query: { ...ctx.request.query } });
+        const req = ctx.request;
+        const logFormat = `\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+Request original url: ${req.originalUrl}
+Method: ${req.method}
+IP: ${req.headers['x-real-ip']}  
+Query: ${JSON.stringify(req.query)}
+Token: ${ctx.headers.authorization}
+Body: ${JSON.stringify(
+          req.body
+        )} \n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n`;
+        ctx.logger.error(logFormat, error);
         if (error.status >= 404) ctx.status = error.status;
         ctx.body = {
           code: error.status || 0,
-          message: error.message
+          message: error.message,
         };
       }
     };
